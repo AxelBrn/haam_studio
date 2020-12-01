87ca8b6bfd48c41db2f62440fd216339423dba38
Hannah <lilypickle@hotmail.fr>
2020-12-01 15:30:51 +0100

    adds header to pages file, adds empty stylesheet, moves hteml head form index.php to header.php, creates home.php to inculde other elements

---
+++ yule/pages/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="public/css/bulma.css" rel="stylesheet">
+    <link href="public/css/stylesheet.css" rel="stylesheet">
+    <title>Yule - Brought to you</title>
+</head>
+<body>
+  <header>
+    <div id="header" class="cotainer">
+      <figure class="image">
+        <img src="/public/images/logo.png">
+      </figure>
+    </div>
+  </header>
