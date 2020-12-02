331603598a856654f8aaafdd3141180c15d93876
Hannah <lilypickle@hotmail.fr>
2020-12-02 10:08:37 +0100

    Css and Html for navbar part

--- yule/pages/header.php
+++ yule/pages/header.php
@@ -3,15 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="public/css/bulma.css" rel="stylesheet">
-    <link href="public/css/stylesheet.css" rel="stylesheet">
+    <link href="/public/css/bulma.css" rel="stylesheet">
+    <link href="/public/css/stylesheet.css" rel="stylesheet">
+    <script src="https://unpkg.com/eva-icons"></script>
     <title>Yule - Brought to you</title>
 </head>
 <body>
   <header>
+
     <div id="header" class="cotainer">
-      <figure class="image">
-        <img src="/public/images/logo.png">
-      </figure>
+      <nav class="navbar is-white p-4 ">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/?page=home">
+            <img src="/public/images/logo.png">
+          </a>
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu ">
+          <div class="navbar-end is-pulled-right">
+            <a href="/?page=fashion" class="navbar-item px-5">Fashion</a>
+            <a href="/?page=food" class="navbar-item px-5">Food</a>
+            <a href="/?page=decoration" class="navbar-item px-5">Decoration</a>
+            <a href="/?page=food" class="navbar-item px-5">News</a>
+            <a href="/?page=contact" class="navbar-item px-5">Contact</a>
+            <a href="/?page=basket" class="navbar-item px-5">
+              <span class="icon">
+                <i data-eva="person-outline"></i>
+              </span>
+            </a>
+            <a href="/?page=account" class="navbar-item px-5">
+              <span class="icon">
+                <i data-eva="shopping-cart-outline"></i>
+              </span>
+            </a>
+            
+              
+
+              
+            </div>
+          </div>
+      </nav>
     </div>
+
   </header>
+
+  <script>
+      eva.replace()
+    </script> 
