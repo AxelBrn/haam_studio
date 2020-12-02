91fbff26dfb62bafc31a8fbba7ac06db8b8dc5ce
Hannah <lilypickle@hotmail.fr>
2020-12-02 16:05:06 +0100

    Start of homepage Adds images for homepage and line drawings Error with footer from previous commit

--- yule/pages/home.php
+++ yule/pages/home.php
@@ -1,4 +1,33 @@
 <?php 
     include("header.php"); 
+ ?>
+
+ <div class="hero is-medium">
+  <div class="hero-body ">
+    <div class="container">
+      <h1 class="title ">
+        Yule
+      </h1>
+      <h2 class="title subtitle">
+        The Christmas Market brought to you
+      </h2>
+    </div>
+  </div>
+</div>
+
+<div> 
+    <div class="container is-flex is-flex-direction-column" >
+        <h2 class="title subtitle has-text-left">Welcome!</h2>
+        <p>Considering the state of the world and the country at the moment local christmas markets have been shut down. To bring you that holiday cheer and local christmas market experience we have decided to bring the markets to you.<br>You can find your favourite market products here on yule. </p>
+    
+    </div>
+    <img class="is-pulled-right" src="/public/images/line/accueil+contact.png">
+</div>
+
+<div class="is-dark">
+    <div></div>
+</div>
+
+<?php
     include("footer.php"); 
 ?>
