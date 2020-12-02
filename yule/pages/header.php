67850680652100aff8c40acfe8f93d630fb9cbcb
Hannah <lilypickle@hotmail.fr>
2020-12-02 14:06:49 +0100

    removes unsed font files Loop for categories on header, connects to DB

--- yule/pages/header.php
+++ yule/pages/header.php
@@ -1,3 +1,6 @@
+<?php
+  require ("repository/categoriesRepository.php")
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/public/css/bulma.css" rel="stylesheet">
     <link href="/public/css/stylesheet.css" rel="stylesheet">
-    <link href="/public/script/script.js">
+    <script src="/public/script/script.js"></script>
     <script src="https://unpkg.com/eva-icons"></script>
     <title>Yule - Brought to you</title>
 </head>
@@ -13,23 +16,26 @@
   <header>
 
     <div id="header" class="cotainer">
-      <nav class="navbar is-white p-4 level ">
+      <nav class="navbar is-white p-4 level" id="navbar">
         <div class="navbar-brand level-item has-text-centered">
-          <a class="navbar-item" href="/?page=home">
+          <a class="navbar-item" href="/">
             <img src="/public/images/logo.png">
           </a>
-          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu" data-target2="navMenuChild" data-target3="navbar">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
-        <div class="navbar-menu ">
-          <div class="navbar-end is-pulled-right level-item">
-            <a href="/?page=fashion" class="navbar-item px-5">Fashion</a>
-            <a href="/?page=food" class="navbar-item px-5">Food</a>
-            <a href="/?page=decoration" class="navbar-item px-5">Decoration</a>
-            <a href="/?page=food" class="navbar-item px-5">News</a>
+        <div class="navbar-menu" id="navMenu">
+          <div class="navbar-end is-pulled-right level-item" id="navMenuChild" >
+<?php
+$categories = getAll();
+  for($i = 0; $i < count($categories); $i++ ){
+    echo ("<a href=\"/?page=".strtolower($categories[$i]->label)."\" class=\"navbar-item px-5\">".$categories[$i]->label."</a>");
+  }
+?>
+            <a href="/?page=news" class="navbar-item px-5">News</a>
             <a href="/?page=contact" class="navbar-item px-5">Contact</a>
             <a href="/?page=basket" class="navbar-item px-5">
               <span class="icon">
--- yule/public/script/script.js
+++ yule/public/script/script.js
@@ -12,11 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
+        const target2 = el.dataset.target2;
+        const target3 = el.dataset.target3;
         const $target = document.getElementById(target);
+        const $target2 = document.getElementById(target2);
+        const $target3 = document.getElementById(target3);
+        
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
+        $target.classList.toggle('navbar-menu');
+        $target2.classList.toggle('is-pulled-right');
+        $target2.classList.toggle('level-item');
+        $target3.classList.toggle('level');
 
       });
     });
