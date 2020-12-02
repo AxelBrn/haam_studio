179182886e2d8b1721c78375586c92647344f503
Axel Brionne <axel.brionne@gmail.com>
2020-12-02 12:05:29 +0100

    first version of footer without text

--- yule/pages/footer.php
+++ yule/pages/footer.php
@@ -2,19 +2,22 @@
             <div class="container is-flex is-justify-content-center is-align-items-center" style="width: 100%; height: 100%;">
                 <div>
                     <h1 class="title has-text-weight-bold has-text-centered is-size-1">Newsletter</h1>
-                    <p style="max-width: 500px">
+                    <p class="mx-5" style="max-width: 500px">
                         Etiam nec odio vitae ligula venenatis pretium. Proin sit amet ligula ultrices, porta libero vel, dignissim lacus.
                         Etiam nec odio vitae ligula venenatis pretium. Proin sit amet ligula ultrices, porta libero vel, dignissim lacus.
                     </p>
-                    <div class="field has-addons is-flex is-justify-content-center mt-6">
-                        <p class="control">
-                            <input class="input" type="email" placeholder="Email">
-                        </p>
-                        <p class="control">
-                            <button class="button has-background-grey-lighter">
-                                <strong>OK</strong>
-                            </button>
-                        </p>
+                    <div class="field has-addons is-flex is-justify-content-center mt-6" >
+                        <div class="field has-addons" style="-webkit-box-shadow: 0px 6px 13px -1px rgba(0,0,0,0.1); 
+                                                            box-shadow: 0px 6px 13px -1px rgba(0,0,0,0.1);">
+                            <p class="control">
+                                <input class="input" type="email" placeholder="Email">
+                            </p>
+                            <p class="control">
+                                <button class="button has-background-grey-lighter">
+                                    <strong>OK</strong>
+                                </button>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
