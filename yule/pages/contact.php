a69ed960f47c0793e212c46215bd45c66f69829c
Hannah <lilypickle@hotmail.fr>
2020-12-03 11:57:51 +0100

    Contact page

---
+++ yule/pages/contact.php
@@ -0,0 +1,39 @@
+<div id="contact">
+    <div class="hero is-medium">
+        <div class="hero-body ">
+            <div class="container">
+              <h1 class="title ">
+                  Contact
+              </h1>
+            </div>
+        </div>
+    </div>
+    <div>
+      <div id="adresses"> 
+          <div class="box box-contact is-flex is-flex-direction-column" >
+              <h2 class="title has-text-weight-bold has-text-left is-size-1">How to contact us</h2>
+              <div class="is-flex is-flex-direction-row  is-flex-wrap-wrap is-justify-content-center">
+                <div class="m-3 is-flex is-flex-direction-row">
+                  <span class="icon">
+                      <i data-eva="email-outline"></i>
+                  </span>
+                  <div class="is-flex is-flex-direction-column ">
+                    <p class="is-size-4 has-text-weight-bold">Our email address</p>
+                    <p>yule@christmasmarket.com</p>
+                  </div>
+                </div>
+                <div class="m-3 is-flex is-flex-direction-row">
+                  <span class="icon">
+                      <i data-eva="phone-outline"></i>
+                  </span>
+                  <div class="is-flex is-flex-direction-column">
+                    <p class="is-size-4 has-text-weight-bold">Our phone</p>
+                    <p>yule@christmasmarket.com</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+      </div>
+      <img src="/public/images/line/accueil+contact.png" style="margin: 0 auto;">
+    </div>
+</div>
