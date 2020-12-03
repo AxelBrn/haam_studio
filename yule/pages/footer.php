54b6ce7d5608c5bf91294f53881ae27f834afb81
Hannah <lilypickle@hotmail.fr>
2020-12-03 10:13:48 +0100

    Text changes to footer

--- yule/pages/footer.php
+++ yule/pages/footer.php
@@ -5,7 +5,7 @@
         
         <div class="box box-products is-flex is-justify-content-center is-align-items-center">
             <div class="mt-5 is-justify-content-center is-align-items-center">
-            <h2 class="title has-text-weight-bold has-text-right is-size-1">The products</h2>
+            <h2 class="title has-text-weight-bold has-text-right is-size-1">Our products</h2>
                 <div class="is-flex is-flex-direction-row is-flex-wrap-wrap is-justify-content-center">
                     <?php
                         for($i = 0; $i < count($categories); $i++) {
@@ -18,7 +18,7 @@
                                 <div class='card-product' style='background-image: url(public/images/footer/".$categories[$i]->card_img."); 
                                  '>
                                     <div class='is-flex is-justify-content-center is-align-items-center max-size transparent-black'>
-                                    <a class='button is-success is-rounded' href='/?page=".strtolower($categories[$i]->label)."&idCateg=".$categories[$i]->id."'>Shop</a>
+                                    <a class='button is-success is-rounded' href='/?page=".strtolower($categories[$i]->label)."&idCateg=".$categories[$i]->id."'>See in shop</a>
                                     </div>
                                 </div>
                                 <p class='has-text-centered mt-4'>".$categories[$i]->label."</p>
@@ -33,10 +33,7 @@
             <div class="container max-size is-flex is-justify-content-center is-align-items-center">
                 <div>
                     <h2 class="title has-text-weight-bold has-text-centered is-size-1">Newsletter</h2>
-                    <p class="mx-5" style="max-width: 500px">
-                        Etiam nec odio vitae ligula venenatis pretium. Proin sit amet ligula ultrices, porta libero vel, dignissim lacus.
-                        Etiam nec odio vitae ligula venenatis pretium. Proin sit amet ligula ultrices, porta libero vel, dignissim lacus.
-                    </p>
+                    <p class="mx-5" style="max-width: 500px">If you wish to be notified when new products launch on yule, don't hesitate to subscribe to our newsletter. We will also keep you updated on any news we have for yule or the christmas markets.</p>
                     <div class="field has-addons is-flex is-justify-content-center mt-6" >
                         <div class="field has-addons elevation">
                             <p class="control">
@@ -44,7 +41,7 @@
                             </p>
                             <p class="control">
                                 <button class="button has-background-grey-lighter">
-                                    <strong>OK</strong>
+                                    <strong>Subscribe</strong>
                                 </button>
                             </p>
                         </div>
@@ -55,19 +52,19 @@
         <footer class="footer is-flex is-justify-content-center is-align-items-center">
             <div class="is-flex is-flex-direction-row mx-6">
                 <div class="content has-text-left mr-5">
-                    <p><span class="has-text-weight-bold has-text-white"> CUSTOMERS SERVICE </span><br>
+                    <p><span class="has-text-weight-bold has-text-white">CUSTOMER SERVICE</span><br>
                     <span class="has-text-white-ter">
-                        Nous contacter<br>
-                        Livraison & suivi <br>
-                        Retours</p>
+                        Contact us<br>
+                        Delivery & Tracking <br>
+                        Returns</p>
                     </span>
                 </div>
                 <div class="content has-text-left">
-                <p><span class="has-text-weight-bold has-text-white">INFORMATIONS PRATIQUES</span> <br>
+                <p><span class="has-text-weight-bold has-text-white">PRATICAL INFORMATION</span> <br>
                     <span class="has-text-white-ter">
-                        Carrières<br>
-                        Mentions Légales<br>
-                        Protection des données</p>
+                        Jobs<br>
+                        Legal mentions<br>
+                        Protection of personnal data</p>
                     </span>
                 </div>
             </div>
