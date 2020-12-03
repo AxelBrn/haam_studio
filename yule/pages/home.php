2dbccd47605f1ef7753986181e8a7071a47ef567
Hannah <lilypickle@hotmail.fr>
2020-12-03 11:27:16 +0100

    All elements for home page TODO if possible position the line drawings

--- yule/pages/home.php
+++ yule/pages/home.php
@@ -1,33 +1,93 @@
-<?php 
-    include("header.php"); 
- ?>
-
- <div class="hero is-medium">
-  <div class="hero-body ">
-    <div class="container">
-      <h1 class="title ">
-        Yule
-      </h1>
-      <h2 class="title subtitle">
-        The Christmas Market brought to you
-      </h2>
+<div id="home">
+    <div class="hero is-medium">
+        <div class="hero-body ">
+            <div class="container">
+            <h1 class="title ">
+                Yule
+            </h1>
+            <h2 class="title subtitle is-size-1">
+                The Christmas Market brought to you
+            </h2>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-
-<div> 
-    <div class="container is-flex is-flex-direction-column" >
-        <h2 class="title subtitle has-text-left">Welcome!</h2>
-        <p>Considering the state of the world and the country at the moment local christmas markets have been shut down. To bring you that holiday cheer and local christmas market experience we have decided to bring the markets to you.<br>You can find your favourite market products here on yule. </p>
-        <img class="" src="/public/images/line/accueil+contact.png">
+    <div id="welcome"> 
+        <div class="box box-home is-flex is-flex-direction-column" >
+            <h2 class="title has-text-weight-bold has-text-left is-size-1">Welcome!</h2>
+            <p >Considering the state of the world and the country at the moment local christmas markets have been shut down. To bring you that holiday cheer and local christmas market experience we have decided to bring the markets to you.<br>You can find your favourite market products here on yule. </p>
+            <img class="" src="/public/images/line/accueil+contact.png">
+        </div>
     </div>
-    
+    <div  id="food" class="black-background">
+        <div class="box box-home black-background is-flex is-flex-direction-column ">
+            
+            <div class="is-flex is-flex-direction-row ">
+                <img class="hide-image" src="/public/images/line/accueil4.png">
+                <div>
+                    <div class="mt-5">
+                        <h2 class="title has-text-weight-bold has-text-right is-size-1">Food</h2>
+                    </div>
+                    <img class="" src="/public/images/homepage/image1.png">
+                    <p>Were you disappointed this year not to be able to eat your favoritechristmas market foods, or get a glass of hot mulled wine ? well we have a solution to your problem. With yule our artisana who usually sell food hav adapted themselves to be able to send all of your christmas market favorites right to your door.</p>
+                    <a class='button is-success is-rounded is-pulled-right' href='/?page=food'>Shop for food</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div  id="decoration" class="">
+        <div class="box box-home is-flex is-flex-direction-column ">  
+            <div class="is-flex is-flex-direction-row ">
+                <div>
+                    <div class="mt-5">
+                        <h2 class="title has-text-weight-bold has-text-left is-size-1">Decorations</h2>
+                    </div>
+                    <img class="display-image" src="/public/images/homepage/image2.png">
+                    <p>Are you looking for decorations to up your decoration game this year ? Well look no further, our christams market artisan are selling the best decorations you can find. From baubles to special ornaments, to tinsel,to christmas lights and even the best Tress you can find. </p>
+                    <a class='button is-success is-rounded' href='/?page=food'>Shop for decorations</a>
+                </div>
+                <img class="hide-image" src="/public/images/homepage/image2.png">
+            </div>
+            <img class="" src="/public/images/line/accueil3.png">
+        </div>
+    </div>
+    <div  id="fashion" class="yellow-background">
+        <div class="box yellow-background box-home is-flex is-flex-direction-column ">  
+            <div class="is-flex is-flex-direction-row ">
+                <img class="hide-image" src="/public/images/homepage/image3.jpg" style="width:30%; margin:auto 1rem;">
+                <div>
+                    <div class="mt-5">
+                        <h2 class="title has-text-weight-bold has-text-right is-size-1">Fashion</h2>
+                    </div>
+                    <p>Do you want the perfect look for christmas day ? Need some new outerwear to keep you warm this winter? well we have the perfect category for you. Check out our fashion products to discover what clothe items our artisans are selling to make you look FABULOUS. </p>
+                    <a class='button is-success is-rounded' href='/?page=food'>Shop in fashion</a>
+                    <img class="hide-image" src="/public/images/line/accueil.png">
+                </div>
+            </div>
+            <div class="is-flex is-flex-direction-column ">
+                <img class="display-image" src="/public/images/homepage/image3.jpg" style="width:50%;margin:0 auto;">
+                <img class="display-image" src="/public/images/line/accueil.png">
+            </div>
+        </div>
+    </div>
+    <div  id="news" class="">
+        <div class="box box-home is-flex is-flex-direction-column ">  
+            <div class="is-flex is-flex-direction-row ">
+                <div>
+                    <div class="mt-5 ">
+                        <h2 class="title has-text-weight-bold has-text-left is-size-1">News</h2>
+                    </div>
+                    <p>Are you looking for decorations to up your decoration game this year ? Well look no further, our christams market artisan are selling the best decorations you can find. From baubles to special ornaments, to tinsel,to christmas lights and even the best Tress you can find. </p>
+                    <a class='button is-success is-rounded' href='/?page=food'>See all news</a>
+                    
+                </div>
+                <img class="hide-image" src="/public/images/line/accueil2.png " style="width:50%;">
+            </div>
+            <img class="display-image" src="/public/images/line/accueil2.png " style="width:100%;">
+            <img class="" src="/public/images/homepage/image4.png" >
+            <div class="mt-5 is-flex is-justify-content-center is-align-items-center">
+                <a class='button is-success is-rounded' href='/?page=food'>Read about Covid measures</a>
+            </div>
+        </div>
+    </div>
+   
 </div>
-
-<div class=" container is-flex is-flex-direction-column is-dark">
-    <div></div>
-</div>
-
-<?php
-    include("footer.php"); 
-?>
